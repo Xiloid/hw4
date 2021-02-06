9408f6d0c80b8f440afa0cbc815a049dc632c364
Xiloid <faustov.roman.a@gmail.com>
2021-02-06 21:55:02 +0200

    111

--- longest_word_.py
+++ longest_word_.py
@@ -3,5 +3,24 @@
     1. Вывести количество слов в введенной строке.
     2. Вывести самое длинное слово и его длину.
 """
-
+count = flag = numlet = ind = big = 0
 string = input('Введите строку: ')
+for i in range(len(string)):
+    if string[i] != ' ' and flag == 0:
+        count += 1
+        flag = 1
+    else:
+        if string[i] == ' ':
+            flag = 0
+    if string[i] != ' ':
+        numlet += 1  # считаем количество букв в слове
+    else:
+        if numlet > big:  # сравниваем длины слов и запоминаем индекс начала
+            big = numlet
+            ind = i - numlet  # индекс начала слова
+        numlet = 0
+if numlet > big:
+    m = numlet
+    ind = i - numlet + 1
+print(f'В строке слов: {count}')
+print(f'Самое длинное слово: "{string[ind:ind + big]}" и в этом слове {big} букв')
